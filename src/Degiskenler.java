272a23755994474fec1c05a3bcafb94606add428
fatmayakut <fatmayakut90@gmail.com>
2017-10-12 15:32:15 +0300

    Initial commit
    Signed-off-by: fatmayakut <fatmayakut90@gmail.com>

--- src/Degiskenler.java
+++ src/Degiskenler.java
@@ -7,10 +7,30 @@
 public class Degiskenler {
     public static void main(String[] args){
         int a=5;
+        int ilkSayim=10;
+        int ikinciSayim=15;
         System.out.println(a);
         System.out.println(a+7);
         System.out.println(8+7);
         System.out.println("fatma yakut java dersleri");
+        System.out.println(ikinciSayim+ilkSayim);
+        System.out.println("Byte" +Byte.MAX_VALUE+Byte.MIN_VALUE+Byte.SIZE);
+        System.out.println("int" +Integer.MAX_VALUE+Integer.MIN_VALUE+Integer.SIZE);
 
+        /**java 7 ile gelen özellik boşluk yada tireleri görmezden gelip birleştirme yapar
+         */
+        int Enbuyukdeger=2_565_123_56;
+        System.out.println("enbuyukdeğer"+Enbuyukdeger);
+        System.out.println("float" +Float.MAX_VALUE+Float.MIN_VALUE+Float.SIZE);
+        float sayi=9.264f;//sonuna f yazılmalıdır
+        double sayi2=9.1;
+        System.out.println("double" +Double.MAX_VALUE+Double.MIN_VALUE+Double.SIZE);
+        System.out.println("float:"+sayi);
+        System.out.println("double:"+sayi2);
+
+       char harf='A';
+       int sayi1=harf;
+        System.out.println("char harf:"+harf+"sayi1:"+sayi1);
+//bir değişkene aynı veri tipteki veriler atılabilir. char olan veriyi int olan değişkende tutulmamalı
     }
 }
