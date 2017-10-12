aa8da26fd283133e98589a8f652ffafaa325f8ff
Fatma YAKUT <fyakut@innova.com.tr>
2017-10-12 15:02:14 +0300

    Initial commit

---
+++ src/Degiskenler.java
@@ -0,0 +1,16 @@
+/**
+ * Created by fyakut on 12.10.2017.
+ * değişken; bellekte saklamak istediğimiz verilerin isimlendirilmesidir.
+ * bellekte ben "5 " sayısısnı saklamak istiyor isem a=5 dediğimde değişkenim "a" olmaktadır.
+ * Ayni tiptekiler aynı değişkene atanabilir.
+ */
+public class Degiskenler {
+    public static void main(String[] args){
+        int a=5;
+        System.out.println(a);
+        System.out.println(a+7);
+        System.out.println(8+7);
+        System.out.println("fatma yakut java dersleri");
+
+    }
+}
